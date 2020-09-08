10e9d4647f07fc9865e96202ac2d4bb31121263b
Miguel Lorenzo <jsmglorenzo@gmail.com>
2020-09-08 00:27:10 -0400

    added other images, fixed form styling and social widgets

--- js/index.js
+++ js/index.js
@@ -1,16 +1,29 @@
 imgArray = [
-    "../assets/img/lauryn.jpg",
-    "../assets/img/city.jpg",
-    "../assets/img/raptors.jpg",
-    "../assets/img/walkman.jpg",
-    "../assets/img/70.jpg",
+    "lauryn.jpg",
+    "walkman.jpg",
+    "raptors.jpg",
+    "rocket.jpg",
+    "city.jpg",
+    "disco.jpg",
+    "coral.jpg",
+    "einstein.jpg",
+    "jayz.jpg",
+    "trees.jpg",
+    "knight.jpg",
+    "city2.jpg",
+    "missy.jpg",
+    "beatles.jpg",
+    "ratm.jpg",
+    "sinatra.jpg",
+    "bubbles.jpg",
+    "vintage.jpg",
 ]
 
 const slideshow = (imgArray, target) => {
     let counter = 0;
     target = document.querySelector(target);
     window.setInterval(() => {
-        target.style.background = `url(${imgArray[counter]})`;
+        target.style.background = `url(../assets/img/slides//${imgArray[counter]})`;
         target.style.backgroundSize = "cover";
         target.style.backgroundPosition = "50% 50%";
         target.style.backgroundRepeat = "none";
