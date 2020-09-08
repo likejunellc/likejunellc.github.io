4aed22a5273e6f86b6b639611e2b7b16c78a819e
Miguel Lorenzo <jsmglorenzo@gmail.com>
2020-09-08 00:52:41 -0400

    rel path hotfix

--- js/index.js
+++ js/index.js
@@ -19,13 +19,13 @@ imgArray = [
     "vintage.jpg",
 ]
 
-const siteURL = "https://likejunellc.github.io/like-june/"
+const siteURL = "https://likejunellc.github.io/like-june"
 
 const slideshow = (imgArray, target) => {
     let counter = 0;
     target = document.querySelector(target);
     window.setInterval(() => {
-        target.style.background = `url(${siteURL}/assets/img/slides//${imgArray[counter]})`;
+        target.style.background = `url(../assets/img/slides//${imgArray[counter]})`;
         target.style.backgroundSize = "cover";
         target.style.backgroundPosition = "50% 50%";
         target.style.backgroundRepeat = "none";
